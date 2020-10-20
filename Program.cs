f88570b6cfba983acbfa057e45f6b76126db60f8
Miguel Romao <miguelrf95@gmail.com>
2020-10-20 01:04:47 +0100

    Remove unwanted folders

--- Program.cs
+++ Program.cs
@@ -8,112 +8,146 @@ namespace CSVReader
     {
         private static void Main(string[] args)
         {
-            string projectPath = @"D:\Miguel\Documents\Projetos\CSVReader\";
+            //     string projectPath = @"D:\Miguel\Documents\Projetos\CSVReader\";
 
-            string csvPath = string.Concat(projectPath, "scr.csv");
-            string resultPath;
-            string answer;
-            bool validInput = false;
-            int window;
+            //     string csvPath = string.Concat(projectPath, "scr.csv");
+            //     string resultPath;
+            //     string answer;
+            //     bool validInput = false;
+            //     int window;
 
 
-            SignalProcessing _signalProcesser = new SignalProcessing(csvPath);
+            //     SignalProcessing _signalProcesser = new SignalProcessing(csvPath);
 
-            Console.WriteLine("There are {0} values to process",
-                                    _signalProcesser.XValues.Length);
-            Console.Write("\nInput the search window: ");
+            //     Console.WriteLine("There are {0} values to process",
+            //                             _signalProcesser.XValues.Length);
+            //     Console.Write("\nInput the search window: ");
 
-            do
+            //     do
+            //     {
+            //         answer = Console.ReadLine();
+            //         if (Int32.TryParse(answer, out window))
+            //         {
+            //             validInput = true;
+            //         }
+            //         else
+            //         {
+            //             Console.WriteLine("Not a valid input...");
+            //         }
+            //     } while (!validInput);
+
+            //     Console.WriteLine("Press a key to find peaks...");
+            //     Console.ReadKey();
+
+            //     _signalProcesser.FindPeaks(window);
+
+            //     Console.WriteLine("There are {0} peaks, with a window range of {1}",
+            //                         _signalProcesser.ListOfMaximums.Count, window);
+
+            //     Console.ReadKey();
+
+            //     foreach (Tuple<float, float> maximum in _signalProcesser.ListOfMaximums)
+            //     {
+            //         System.Console.WriteLine(
+            //             "Peak{0} and position {1}", maximum.Item2, maximum.Item1);
+            //     }
+
+            //     Console.WriteLine("There are {0} minimums, with a window range of 70",
+            //                         _signalProcesser.ListOfMinimus.Count);
+
+            //     Console.ReadKey();
+            //     Console.WriteLine();
+
+            //     foreach (Tuple<float, float> minimums in _signalProcesser.ListOfMinimus)
+            //     {
+            //         Console.WriteLine("Minimum{0} and position {1}",
+            //                             minimums.Item2, minimums.Item1);
+            //     }
+
+            //     validInput = false;
+
+            //     Console.Write("\n Want to write values to CSV? ");
+
+            //     do
+            //     {
+            //         answer = Console.ReadLine();
+            //         answer = answer.ToLower();
+            //         if (answer == "y" || answer == "n")
+            //         {
+            //             validInput = true;
+            //         }
+            //         else
+            //         {
+            //             Console.WriteLine("Not a valid input...");
+            //         }
+            //     } while (!validInput);
+
+            //     if (answer == "y")
+            //     {
+            //         string fileName;
+            //         Console.Write("Please enter file name: ");
+
+            //         do
+            //         {
+            //             fileName = Console.ReadLine();
+            //         } while (fileName == null);
+
+            //         resultPath = string.Concat(projectPath, fileName, ".csv");
+
+            //         FileStream fs = new FileStream(resultPath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            //         using (StreamWriter file = new StreamWriter(fs))
+            //         {
+            //             file.WriteLine("# Maximum values");
+            //             for (int i = 0; i < _signalProcesser.ListOfMaximums.Count; i++)
+            //             {
+            //                 file.WriteLine("{0}\t{1}",
+            //                                 _signalProcesser.ListOfMaximums[i].Item1,
+            //                                 _signalProcesser.ListOfMaximums[i].Item2);
+            //             }
+
+            //             file.WriteLine("# Minimum values");
+            //             for (int i = 0; i < _signalProcesser.ListOfMinimus.Count; i++)
+            //             {
+            //                 file.WriteLine("{0}\t{1}",
+            //                                 _signalProcesser.ListOfMinimus[i].Item1,
+            //                                 _signalProcesser.ListOfMinimus[i].Item2);
+            //             }
+            //         }
+            //         fs.Close();
+            //     }
+            System.Random random = new Random();
+
+            List<float> testValues = new List<float>();
+            IEnumerable<Tuple<int, float>> maximums;
+            List<Tuple<int, float>> listOfMaximums = new List<Tuple<int, float>>();
+
+            for (int i = 0; i < 100; i++)
             {
-                answer = Console.ReadLine();
-                if (Int32.TryParse(answer, out window))
-                {
-                    validInput = true;
-                }
-                else
-                {
-                    Console.WriteLine("Not a valid input...");
-                }
-            } while (!validInput);
-
-            Console.WriteLine("Press a key to find peaks...");
-            Console.ReadKey();
-
-            _signalProcesser.FindPeaks(window);
-
-            Console.WriteLine("There are {0} peaks, with a window range of {1}",
-                                _signalProcesser.ListOfMaximums.Count, window);
-
-            Console.ReadKey();
-
-            foreach (Tuple<float, float> maximum in _signalProcesser.ListOfMaximums)
-            {
-                System.Console.WriteLine(
-                    "Peak{0} and position {1}", maximum.Item2, maximum.Item1);
+                float x = (float)random.NextDouble();
+                testValues.Add(x);
             }
 
-            Console.WriteLine("There are {0} minimums, with a window range of 70",
-                                _signalProcesser.ListOfMinimus.Count);
+            maximums = Test.LocalMaxima(testValues, 10);
 
-            Console.ReadKey();
-            Console.WriteLine();
-
-            foreach (Tuple<float, float> minimums in _signalProcesser.ListOfMinimus)
+            foreach(var tuple in maximums)
             {
-                Console.WriteLine("Minimum{0} and position {1}",
-                                    minimums.Item2, minimums.Item1);
+                listOfMaximums.Add(tuple);
             }
 
-            validInput = false;
+            System.Console.WriteLine("The list of floats is:");
 
-            Console.Write("\n Want to write values to CSV? ");
-
-            do
+            foreach (var item in testValues)
             {
-                answer = Console.ReadLine();
-                answer = answer.ToLower();
-                if (answer == "y" || answer == "n")
-                {
-                    validInput = true;
-                }
-                else
-                {
-                    Console.WriteLine("Not a valid input...");
-                }
-            } while (!validInput);
+                System.Console.WriteLine(item);
+            }
+            
+            System.Console.WriteLine();
+            System.Console.WriteLine("Press any key to show list maximums...");
+            Console.ReadKey();
 
-            if (answer == "y")
+            foreach (var item in listOfMaximums)
             {
-                string fileName;
-                Console.Write("Please enter file name: ");
-
-                do
-                {
-                    fileName = Console.ReadLine();
-                } while (fileName == null);
-
-                resultPath = string.Concat(projectPath, fileName, ".csv");
-
-                FileStream fs = new FileStream(resultPath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
-                using (StreamWriter file = new StreamWriter(fs))
-                {
-                    file.WriteLine("# Maximum values");
-                    for (int i = 0; i < _signalProcesser.ListOfMaximums.Count; i++)
-                    {
-                        file.WriteLine("{0}\t{1}",
-                                        _signalProcesser.ListOfMaximums[i].Item1,
-                                        _signalProcesser.ListOfMaximums[i].Item2);
-                    }
-
-                    file.WriteLine("# Minimum values");
-                    for (int i = 0; i < _signalProcesser.ListOfMinimus.Count; i++)
-                    {
-                        file.WriteLine("{0}\t{1}",
-                                        _signalProcesser.ListOfMinimus[i].Item1,
-                                        _signalProcesser.ListOfMinimus[i].Item2);
-                    }
-                }
-                fs.Close();
+                System.Console.WriteLine(item);
             }
         }
     }
---
+++ Test.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSVReader
+{
+    public static class Test
+    {
+        public static IEnumerable<Tuple<int, float>> LocalMaxima(IEnumerable<float> source, int windowSize)
+        {
+            //* Round up to nearest odd value
+            windowSize = windowSize - windowSize % 2 + 1;
+            int halfWindow = windowSize / 2;
+
+            int index = 0;
+            //* where last compared value is gonna go
+            Queue<float> lastComparedValue = new Queue<float>(Enumerable.Repeat(float.NegativeInfinity, halfWindow));
+
+            //* Left half of the window
+            Queue<float> leftHalf = new Queue<float>(source.Take(halfWindow + 1));
+
+            //* goes through collection from half point + 1 
+            foreach (float d in source.Skip(halfWindow + 1).Concat(Enumerable.Repeat(float.NegativeInfinity, halfWindow + 1)))
+            {
+                //* Removes FIFO element from the queue
+                float curVal = leftHalf.Dequeue();
+
+                //* All() : Determines whether all elements of a sequence satisfy a condition.
+                if (lastComparedValue.All(x => curVal > x) && leftHalf.All(x => curVal >= x))
+                {
+                    System.Console.WriteLine($"curVal after compare: {curVal}");
+                    yield return Tuple.Create(index, curVal);
+                }
+
+                //* Removes first instance of Negative Infinity and eventually previous stored curVal
+                lastComparedValue.Dequeue();
+                //* Add the current analysed value
+                lastComparedValue.Enqueue(curVal);
+                //* 
+                leftHalf.Enqueue(d);
+                index++;
+            }
+        }
+    }
+}
